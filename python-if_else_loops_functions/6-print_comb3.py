ae1bce334b8094688d2a3f09e00b04ba5c68ecb6
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-02 17:04:18 +0000

    syntax error

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j) if i < 8 or j < 9 else "{:02d}\n".format(i * 10 + j), ends="")
+        print("{:02d}, ".format(i * 10 + j) if i < 8 or j < 9 else "{:02d}\n".format(i * 10 + j), end="")
