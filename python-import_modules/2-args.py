6f50b195fbeb74a6897cc4942ef5b3fdb1c9ed5d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 02:18:36 +0000

    added error for print statement

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("Number of argument(s): 0.")
     else:
         print("Number of argument(s): {} argument{}".format(num_args, "" if num_args == 1 else "s"))
-        print(":")
+        print("1 argument:")
         for i in range (1, len(argv)):
             print("{}: {}".format(i, argv[i]))
