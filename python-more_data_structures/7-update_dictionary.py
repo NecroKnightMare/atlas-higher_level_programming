26544dc49f84572001e2fd94b4bc0a4e7f43699c
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 00:43:41 +0000

    replaces or adds key/value in a dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if new_dict is not None:
+        print_sorted_dictionary(new_dict)
+    else:
+        print("The dictionary is None")
