39a620543f4296d80fa75f55dc38fe48847fdf48
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 02:29:44 +0000

    changed arg argument to 1 of

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,12 +3,12 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv
-    num_args = len(argv) - 1
+    argv = sys.argv[1:]
+    num_args = len(argv)
 
     if num_args == 0:
-        print("Number of arguments: 0.")
+        print("Number of argument(s): 0.")
     else:
         print("{} argument:{}".format(num_args, "" if num_args == 1 else "s"))
-        for i in range (1, len(argv)):
+        for i, arg in enumerate(argv, start=1):
             print("{}: {}".format(i, argv[i]))
