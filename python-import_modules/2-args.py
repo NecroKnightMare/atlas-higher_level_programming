4c81ca306af9e6857fc5fb94dc9f4a2a919fa055
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 02:33:26 +0000

    changed arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     if num_args == 0:
         print("Number of argument(s): 0.")
     else:
-        print("{} argument:{}".format(num_args, "" if num_args == 1 else "s"))
+        print(f"Number of argument(s): {num_args} argument{'s'if num_args > 1 else ''}:")
         for i, arg in enumerate(argv, start=1):
-            print("{}: {}".format(i, argv[i]))
+            print(f"{i}: {arg}")
