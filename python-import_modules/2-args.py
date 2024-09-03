aaef454f48071bfc7ddb373fa1aa351f6c6146fb
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 02:20:41 +0000

    removed addition and print format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     if num_args == 0:
         print("Number of argument(s): 0.")
     else:
-        print("Number of argument(s): {} argument{}".format(num_args, "" if num_args == 1 else "s"))
-        print("1 argument:")
+        print("{} argument{}".format(num_args, "" if num_args == 1 else "s"))
         for i in range (1, len(argv)):
             print("{}: {}".format(i, argv[i]))
