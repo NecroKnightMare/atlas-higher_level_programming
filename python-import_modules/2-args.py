70cfed9bcfac74d9859654681faa41344c1d4775
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 02:36:29 +0000

    added .

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     num_args = len(argv)
 
     if num_args == 0:
-        print(f"{num_args} arguments")
+        print(f"{num_args} arguments.")
     else:
         print(f"{num_args} argument{'s'if num_args > 1 else ''}:")
         for i, arg in enumerate(argv, start=1):
