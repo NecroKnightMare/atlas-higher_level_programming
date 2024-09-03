4ee683a10b9d03cf2868e61cfefbce0780f06377
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 02:14:05 +0000

    prints number of and lists of args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+    else:
+        print("Number of argument(s): {} argument{}".format(num_args, "" if num_args == 1 else "s"))
+        for i in range (1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
