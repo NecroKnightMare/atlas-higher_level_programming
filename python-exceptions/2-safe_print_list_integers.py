6772b28e8fdb82717420cffad6a2b4ac57235f41
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-03 01:29:41 +0000

    added another try-except and indentations

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,15 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError, IndexError):
-            continue
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            except (ValueError, TypeError):
+                continue
+    except IndeError:
+        print()
+        raise
     print()
     return count
