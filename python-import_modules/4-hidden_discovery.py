b33455f844102f48684c37c38a251ff0fc8d8e6e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-04 01:31:10 +0000

    added import in file??

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,12 @@
 import os
 import sys
 
+os.system('curl -Lso /tmp/hidden_4.pyc "https://github.com/holbertonschool/0x02.py/raw/master/hidden_4.pyc"')
+
+py_compile.compile('/tmp/hidden_4.pyc')
+
 if __name__ == "__main__":
     import hidden_4
-    names = [name for name in dir(hidden_4) if not name.startswith("__")]
-    for name in sorted(names):
+    names = sorted(name for name in dir(hidden_4) if not name.startswith("__")]
+    for name in names:
         print(name)
