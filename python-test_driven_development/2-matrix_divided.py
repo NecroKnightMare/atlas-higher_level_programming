00e3c8e3fc68491ae158d9d34cdf32a14bde0447
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-10 23:11:24 +0000

    indentation error?

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-divide a matrix
-"""
+
 def matrix_divided(matrix, div):
     """
     Args:
@@ -18,7 +16,8 @@ def matrix_divided(matrix, div):
         list of floats divided to .2
 
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for
+row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     for row in matrix:
