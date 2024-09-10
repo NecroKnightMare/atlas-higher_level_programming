63b3485c290df353ce7dc0a92b5405186340a547
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-10 22:07:05 +0000

    didnt input position argument in init

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,7 +14,7 @@ class Square:
         size: gets or sets size of square
         area: calcs area of square
 """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
