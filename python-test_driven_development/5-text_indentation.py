2a83835c4176d52210be0c04362f88dbaa781c1a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 00:34:58 +0000

    intranet test not needed, file creates 2 lines after . ? :

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+prints text with 2 lines after . ? :
+"""
+def text_indentation(text):
+    """
+    Args:
+        text: string
+
+    Raises:
+        TypeError: if not a string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    result = ""
+    i = 0
+
+    while i < len(text):
+        result += text[i]
+        if text[i] in special_chars:
+            result += "\n\n"
+            i =+ 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
+
+    print(result, end="")
