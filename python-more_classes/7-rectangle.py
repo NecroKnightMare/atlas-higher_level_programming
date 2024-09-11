1e469871f8ef0718ab240421eed0063faba4d40d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 02:48:00 +0000

    updated init and str

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
-        Rectangle.print_symbol = "#"
+        self.print_symbol = "#"
 
     @property
     def width(self):
@@ -63,7 +63,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(Rectangle.print_symbol)* self.__width for _ in range(self.__height)])
+        return "\n".join([str(self.print_symbol)* self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
