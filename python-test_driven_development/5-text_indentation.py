6fef013efb5ba8d28755fc9ff6182b04d121c686
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 00:41:38 +0000

    had += backwards

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -13,14 +13,14 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    
     result = ""
     i = 0
-
     while i < len(text):
         result += text[i]
         if text[i] in ".?:":
             result += "\n\n"
-            i =+ 1
+            i += 1
             while i < len(text) and text[i] == ' ':
                 i += 1
             continue
