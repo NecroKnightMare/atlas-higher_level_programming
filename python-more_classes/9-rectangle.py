06d30783eb62ba7ac65f0c96200d1bc52ad3f6fc
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 03:06:53 +0000

    added condition if width  is less than 0

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -82,4 +82,6 @@ class Rectangle:
         return rect_2
     @classmethod
     def square(cls, size=0):
+        if size < 0:
+            raise ValueError("width must be >= 0")
         return cls(size,size)
