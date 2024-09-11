c99848d04216e615d3c397d8fd4216ddae727329
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 01:20:26 +0000

    added more tests and changed import

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,7 +3,7 @@
 unittests
 """
 import unittest
-from max_integer_6 import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
        """
@@ -12,18 +12,15 @@ class TestMaxInteger(unittest.TestCase):
     """
     def test_positive_integers(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_negative_integers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_mixed_integers(self):
         self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
-        self.assertEqual(max_integer([1, -2, 3, -4]), 3)
 
     def test_single_element(self):
-        self.assertEqual(max_integer([7]), 7)
+        self.assertEqual(max_integer([9]), 9)
 
     def test_empty_list(self):
         self.assertIsNone(max_integer([])
@@ -35,6 +32,15 @@ class TestMaxInteger(unittest.TestCase):
     def test_not_integer_elements(self):
         with self.assertRaises(TypeError):
             max_integer([1, "two", 3])
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.5, 2.7, 3.8, 4.2]), 4.2)
+
+    def test_repeat_max(self):
+        self.assertEqual(max_integer([1, 2, 3, 3, 2, 1]), 3)
+
+    def test_large(self):
+        self.assertEqual(max_integer([1000000, 2000000, 3000000]), 3000000)
 
 if __name__ == '__main__':
     unittest.main()
