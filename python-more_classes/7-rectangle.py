a34de88d39306b7a7ffb56563d90ce3a67307799
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 02:44:37 +0000

    updated __str__ method to print symbol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        return "\n".join([str(Rectangle.print_symbol)* self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
