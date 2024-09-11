30548f124481de05fd48f2ea5b58dd8d0e3ef885
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-11 03:37:50 +0000

    will see if this overflow for floats tests well

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,12 +15,18 @@ def add_integer(a, b=98):
 
     Raises:
         TE if not integers
+        OverflowError: float overflow
     """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if isinstance(a, float) and (a > float('inf') or a < float('-inf')):
+        raise OverflowError("a is too large")
+    if isinstance(b, float) and (b > float('inf') or b < float('-inf')):
+        raise OverflowError("b is too large")
 
     a = int(a)
     b = int(b)
