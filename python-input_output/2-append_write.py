664b8fbcc49413ec46fadf7a66f2c877a66502b5
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 22:17:13 +0000

    append to file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""
+file mod
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    append file
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
