bb0dfa5186c684cf577005370ad50e569a18dee0
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 20:47:08 +0000

    basegeometry with area parameter and integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    def area(self):
-        raise
-    def integer_validator(self, name, value):
+"""
+class basegeometry
+"""
 
+
+class BaseGeometry:
+    """
+    paramter:
+        area: create area
+        integer_validator: creates value
+
+    Raise:
+        Exception
+        TyperError
+        ValueError
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{name} must be an integer")
+        if value <= 0:
+            raise ValueError("{name} must be greater than 0")
