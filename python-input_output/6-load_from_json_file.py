3bf8a6aad229a9b2af837fe9a469ebab2ba5ce9a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 22:31:48 +0000

    load obj to file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,9 +5,11 @@ json file mod
 
 
 import json
+
+
 def load_from_json_file(filename):
     """
     load from json
     """
     with open(filename, "r") as file:
-        return json.load(filename)
+        return json.load(file)
