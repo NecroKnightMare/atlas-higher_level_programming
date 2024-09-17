06be7cd876ed6a2535f3e6e0155a65755148dd49
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 23:00:46 +0000

    student class with attrs and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+
+class Student:
+    """
+    Attr:
+        first: name
+        last: name
+        age
+    Methods:
+        to_json: returns
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs 
+                    if has attrs(self, attr)}
+    
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
