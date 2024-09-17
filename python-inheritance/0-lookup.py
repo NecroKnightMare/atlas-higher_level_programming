a189f2ed4365056401d99993cac8dbb54563765b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 18:21:00 +0000

    function that returns the list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-ret a list object
+function  to print an obj list
 """
 def lookup(obj):
+    """
+    returns a list object
+    """
     try:
         return sorted(dir(obj))
     except Exception as e:
