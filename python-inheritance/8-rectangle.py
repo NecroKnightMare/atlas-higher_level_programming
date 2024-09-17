67542438a11dadbed563aa634853e2b83c789d54
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 21:36:11 +0000

    rectangle inherits base, with parameters

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -35,3 +35,9 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
