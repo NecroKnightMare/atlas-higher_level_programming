cf6830b227395739fdcb784e75293d46c10ac3a7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 20:58:45 +0000

    basegeometry with area parameter and integer validator and rectangle class that inherits from base

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -22,8 +22,7 @@ class BaseGeometry:
         if not isinstance(value, int):
             raise TypeError("{name} must be an integer")
         if value <= 0:
-            raise ValueError("{name} must be greater than 0")
-~                                                              
+            raise ValueError("{name} must be greater than 0")                                    
 class Rectangle(BaseGeometry):
     """
     initiation of
