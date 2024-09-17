70b863181bafd3082b9fbbd5748832352cc63d73
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 22:07:19 +0000

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+file mod
+"""
+
+
 def read_file(filename=""):
+    """
+    read file
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
