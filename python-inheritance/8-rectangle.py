b9e1ceeac11e7ec332e7b9fdcebe92cb0cce1cef
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 21:52:22 +0000

    forgot to format

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -17,29 +17,27 @@ class BaseGeometry:
     """
     def area(self):
         raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError("{name} must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
     """
     class rectangle
     """
-    class Rectangle(BaseGeometry):
+class Rectangle(BaseGeometry):
         """
         initiation of
         Parameters:
             width
             height
         """
-        def __init__(self, width, height):
+    def __init__(self, width, height):
             self.integer_validator("width", width)
             self.integer_validator("height", height)
             self.__width = width
             self.__height = height
 
-        def __str__(self):
+    def __str__(self):
             return f"[Rectangle] {self.__width}/{self.__height}"
-
-        def area(self):
-            return self.__width * self.__height
