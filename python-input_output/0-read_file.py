29cf1afffcf2ed00d8d832ae0490541f2e1c4509
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 23:21:42 +0000

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,9 +8,5 @@ def read_file(filename=""):
     """
     read file
     """
-    try:
-        with open(filename, 'r', encoding="utf-8") as file:
+    with open(filename, 'r', encoding="utf-8") as file:
             print(file.read().strip())
-
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
