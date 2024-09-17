57a91e5ebfd3d66b28abc62b7b2ce495c4317e49
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 21:58:43 +0000

    indent

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -23,21 +23,22 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-    """
-    class rectangle
-    """
+"""
+class rectangle
+"""
 class Rectangle(BaseGeometry):
-        """
-        initiation of
-        Parameters:
-            width
-            height
-        """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    """
+    initiation of
+    Parameters:
+        width
+        height
+    """
+def __init__(self, width, height):
+
+    self.integer_validator("width", width)
+    self.integer_validator("height", height)
+    self.__width = width
+    self.__height = height
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
