3e004901083c1a0acf94972976f54d2ac4a8e67f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 22:23:46 +0000

    save obj into file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""
+file json mod
+"""
+
+
 def save_to_json_file(my_obj, filename):
+    """
+    save obj to file
+    """
+    import json
+    with open(filename, "w") as file:
+        json.dump(my_obj, filename)
