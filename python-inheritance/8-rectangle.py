7320811bf1949b34c6b5e73b47f03679e98daf79
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 21:38:07 +0000

    rectangle inherits base, with parameters

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -23,21 +23,21 @@ class BaseGeometry:
             raise TypeError("{name} must be an integer")
         if value <= 0:
             raise ValueError("{name} must be greater than 0")
-class Rectangle(BaseGeometry):
-    """
-    initiation of
-    Parameters:
-        width
-        height
-    """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    class Rectangle(BaseGeometry):
+        """
+        initiation of
+        Parameters:
+            width
+            height
+        """
+        def __init__(self, width, height):
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+            self.__width = width
+            self.__height = height
 
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        def __str__(self):
+            return f"[Rectangle] {self.__width}/{self.__height}"
 
-    def area(self):
-        return self.__width * self.__height
+        def area(self):
+            return self.__width * self.__height
