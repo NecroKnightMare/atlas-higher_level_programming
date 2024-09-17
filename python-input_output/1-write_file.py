dc890a1508da85aadb6aeb2bc743509ffa3874a1
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 22:12:31 +0000

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""
+file mod
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    write into file
+    """
+    with open (filename, "w", encoding="utf-8") as file:
+        return file.write(text)
