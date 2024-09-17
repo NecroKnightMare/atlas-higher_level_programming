5c383f5b228c9c6aa39da93f68494ecd7fa6346b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 20:41:56 +0000

    basegeometry with area parameter

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+class basegeometry
+"""
+
+
 class BaseGeometry:
+    """
+    paramter:
+        area: create area
+
+    Raise:
+        Exception
+    """
     def area(self):
         raise Exception("area() is not implemented")
