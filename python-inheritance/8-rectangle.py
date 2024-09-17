cc9a327ab449bdc7c16ed847e758f2ccc4a716b4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 20:54:30 +0000

    basegeometry with area parameter and integer validator and rectangle class that inherits from base

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,3 +1,32 @@
 #!/usr/bin/python3
+"""
+class basegeometry
+"""
+
+
+class BaseGeometry:
+    """
+    paramter:
+        area: create area
+        integer_validator: creates value
+
+    Raise:
+        Exception
+        TyperError
+        ValueError
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{name} must be an integer")
+        if value <= 0:
+            raise ValueError("{name} must be greater than 0")
+~                                                              
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
+        self.integer__validator("width", width)
+        self.integer__validator("height", height)
+        self.__width = width
+        self.__height = height
+
