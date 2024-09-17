4014a2c66fbb5f7ef7d10d9fd69c297765dab4bf
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-17 22:47:17 +0000

    student class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,15 +5,15 @@ class student
 
 
 class Student(self):
-"""
-Attr:
-    first: name
-    last: name
-    age
-Methods:
-    to_json: returns
-"""
-def __init__(self, first_name, last_name, age):
+    """
+    Attr:
+        first: name
+        last: name
+        age
+    Methods:
+        to_json: returns
+    """
+    def __init__(self, first_name, last_name, age):
     self.first_name = first_name
     self.last_name = last_name
     self.age = age
