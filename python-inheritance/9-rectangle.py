28b668ff62e0025763781d37b7d18a46094aa208
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-18 00:39:27 +0000

    rectangle class with super() and area

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -19,9 +19,9 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError("{name} must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
     """
     class rectangle
     """
@@ -33,8 +33,8 @@ class BaseGeometry:
             height
         """
         def __init__(self, width, height):
-            self.integer_validator("width", width)
-            self.integer_validator("height", height)
+            super().integer_validator("width", width)
+            super().integer_validator("height", height)
             self.__width = width
             self.__height = height
 
