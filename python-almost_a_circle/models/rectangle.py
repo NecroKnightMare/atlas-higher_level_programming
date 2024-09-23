97cd5547fff0fcb491f70a26922e0f735aba60c1
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-23 21:08:13 +0000

    adding value errors with improvement

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -30,11 +30,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.__width = value
-        if value is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
+         self.__width = value
 
     @property
     def height(self):
@@ -42,11 +42,11 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.__height = value
-        if value is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -54,15 +54,18 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.__x = value
+        if not isinstance(value, int):
         if x < 0:
             raise ValueError("x must be >= 0")
+        self.__x = value
+
     @property
     def y(self):
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.__y = value
+        if not isinstance(value, int):
         if y < 0:
             raise ValueError("y must be >= 0")
+        self.__y = value
