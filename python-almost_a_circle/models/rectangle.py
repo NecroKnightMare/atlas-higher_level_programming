4ea73f0f813d5c044f7a7e259f3185f64de41509
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-23 22:36:52 +0000

    erased whitetrail

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,7 +90,7 @@ class Rectangle(Base):
             rectangle int: values and area of rectangle
         """
         return self.width * self.height
-    
+
     def display(self):
         """
         print # repr rectangle
