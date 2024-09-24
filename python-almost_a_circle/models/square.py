f1d83a3663c888ed28de18d73b212cc45e8a9361
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-24 01:17:00 +0000

    Square class that imports from rectangle that imports from base

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 Square class that imports rectangle
 """
 
+
 class Square(Rectangle):
     """
     Attributes:
@@ -21,4 +22,8 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """
+        Returns:
+            string
+        """
         return f"[Square] ({Square}) {self.x}/{self.y} - {self.width}"
