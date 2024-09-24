19d8693cd60045c80506abc38a4a160f1e9ac8d7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-24 23:38:27 +0000

    indent was in rectangle file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -127,4 +127,4 @@ class Rectangle(Base):
                 }
 
         def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+            return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
