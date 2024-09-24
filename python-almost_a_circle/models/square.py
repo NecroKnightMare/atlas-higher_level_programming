4bdfd84073f31b21b35f6d52db15e82d3d181346
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-24 01:46:37 +0000

    doc

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,7 +3,7 @@
 from models.rectangle import Rectangle
 
 """
-Square class that imports rectangle
+Module that def Square class that inherits rectangle
 """
 
 
@@ -15,12 +15,22 @@ class Square(Rectangle):
         size: size of square
         x: x value
         y: y value
-        id: id of square
+        id: id is None
 
     Method:
         string
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        inits Square instance
+
+        Args:
+            size: size of sq
+            x: int val
+            y: int val
+            id: id is None
+            
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
