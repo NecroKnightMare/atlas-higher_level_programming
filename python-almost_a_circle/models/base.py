592f1ad882e842a08c713cd1f8feee99db4fe9ad
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-24 23:43:33 +0000

    update base sile, add json represntation of dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,3 +23,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Method to share data representation
+
+        Args:
+            list_dictionaries
+
+        Return:
+            json rep of arg
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
