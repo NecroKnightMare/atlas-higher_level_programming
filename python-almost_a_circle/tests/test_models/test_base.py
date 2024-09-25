73fdf5c66f85b2af79624990d33314e3e9777e05
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-25 00:34:45 +0000

    created methods to test rect, sq and load from file. imported files and unittest

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,14 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class test_base(unittest.TestCase):
+    def test_create_rectangle(self):
+
+    def test_create_square(self):
+
+    def test_load_from_file(self):
+
+if __name__ == '__main__':
+    unittest.main()
