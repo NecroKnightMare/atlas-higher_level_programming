db2dca6ee811238ba4de8c4296c75230f5ae3242
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-25 01:48:21 +0000

    test method for rectangle

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,7 +5,13 @@ from models.square import Square
 
 class test_base(unittest.TestCase):
     def test_create_rectangle(self):
-
+    rect_dict = {'id': 1, 'width': 3, 'height': 4, 'x': 0, 'y': 0}
+    rect = Rectangle.create(**rect_dict)
+    self.assertEqual(rect.width, 3)
+    self.assertEqual(rect.height, 4)
+    self.assertEqual(rect.x, 0)
+    self.assertEqual(rect.y, 0)
+    
     def test_create_square(self):
 
     def test_load_from_file(self):
