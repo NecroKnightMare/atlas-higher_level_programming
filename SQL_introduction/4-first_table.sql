9fe0070b17fd3de78959d8ee2def992ae4cb8520
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-14 14:15:56 -0500

    entered id and name

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,4 @@
--- needs fixing /creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table;
+-- creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table
+id INT
+name VARCHAR(256);
