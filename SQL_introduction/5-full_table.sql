84a2672ba6666b1fafbcda8b355641d3fb49cb8d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-14 14:51:26 -0500

    prints description  of the table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- prints description of table first_table from hbtn_0c_0
-DESCRIBE first_table FROM hbtn_0c_0;
+-- prints description of table first_table from hbtn_0c_
+SHOW FULL COLUMNS FROM firt_table IN hbtn_0c_0;
