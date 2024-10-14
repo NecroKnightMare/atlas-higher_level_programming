79122067c04cc57d43f3f652a8a44b5b5da4639f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-14 16:33:30 -0500

    anotheer way to ptint description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- prints description of table first_table from hbtn_0c_
-SHOW FULL COLUMNS FROM first_table IN hbtn_0c_0;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
