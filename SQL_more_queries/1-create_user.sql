713dfbfed30e05e7f44b884ec7f7957365193e32
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 16:55:41 -0500

    didn't uppercase create

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create user if doesn't exist that has all priveleges and set password
-Create USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVELEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVELEGES;
