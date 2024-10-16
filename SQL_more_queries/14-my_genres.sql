ebe8094d93ae02f3f430f86a6c31da522c44d577
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 21:38:28 -0500

    created genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,9 @@
 -- import from tv shows, list all dexter shows
+CREATE TABLE genres (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+
 SELECT genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
