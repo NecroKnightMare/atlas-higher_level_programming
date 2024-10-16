33820101bf8965c8845f3acbefe1a59d0d794441
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-16 05:35:25 +0000

    can't think anymore

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 defines City class which links to city table
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
@@ -13,19 +14,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-"""
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
-
-Base = declarative_base()
-
-class City(Base):
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    def __repr__(self):
-        return f"<City(name='{self.name}', state_id={self.state_id})>"
