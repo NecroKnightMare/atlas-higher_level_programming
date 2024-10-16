19f3d30b8ce4e06e14f1f8c86ff7dec6f7ddc271
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 20:44:33 -0500

    fixed syntax

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- 
-    SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+    SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
     FROM genres
     JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
     GROUP BY genres.name
-    HAVING COUNT(tv_show_genres.tv_show_id) > 0
+    HAVING COUNT(tv_show_genres.show_id) > 0
     ORDER BY number_of_shows DESC;
