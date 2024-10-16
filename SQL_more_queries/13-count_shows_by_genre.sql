5bc922b7c53c2912527f2cda84f44890494f09eb
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 20:58:33 -0500

    erased syntax error for genre name

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
     SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
     FROM genres
     LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-    GROUP BY genres.genre_name
+    GROUP BY genres.name
     HAVING COUNT(tv_show_genres.tv_show_id) > 0
     ORDER BY number_of_shows DESC;
