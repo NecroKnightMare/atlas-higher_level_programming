b60d588935b68846177f21f3e6ed2915facc5aa1
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-16 04:39:17 +0000

    added map funsction and str data type

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,20 +8,19 @@ import sys
 def list_cities_by_state(username, password, database, state_name):
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
-    query = """
-        SELECT cities.id, cities.name
+    parameter = """
+        SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC
         """
-    cursor.execute(query, (state_name,))
+    cursor.execute(parameter, (state_name,))
     cities = cursor.fetchall()
     city_names = [city[0] for city in cities]
-    print(", ".join(city_names))
+    print(", ".join(map(str,city_names)))
     cursor.close()
     db.close()
 
 if __name__ == "__main__":
     list_cities_by_state(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-
