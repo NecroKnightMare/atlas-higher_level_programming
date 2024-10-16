9aff8a8a9c63816da2c1e8ce60ce4ee9b706c23b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-16 05:01:58 +0000

    changed for update to name

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-adds the State object "Louisiana" to usa db
+changes the name of a State object from usa db
 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def add_state(username, password, database):
+def update_state_name(username, password, database):
     engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="Louisiana")
-    session.add(new_state)
-    session.commit()
-    print(new_state.id)
-    
+    state_to_update = session.query(State).filter(State.id == 2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+
     session.close()
 
 if __name__ == "__main__":
-    add_state(sys.argv[1], sys.argv[2], sys.argv[3])
+    update_state_name(sys.argv[1], sys.argv[2], sys.argv[3])
