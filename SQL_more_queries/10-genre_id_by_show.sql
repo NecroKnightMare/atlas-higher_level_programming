909dba21d0a610f8b2566ac3f661d3f4ce953f4d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 20:04:12 -0500

    fixed syntax errors for genre id and show id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- import database dump tvshows, list all shows 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres_id ASC;
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
