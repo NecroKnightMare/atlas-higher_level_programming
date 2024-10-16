185a49246dbf90951e7d17a37b03680e672d4353
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 21:41:46 -0500

    tv_genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,7 @@
 -- import from tv shows, list all dexter shows
-CREATE TABLE genres (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
-
-SELECT genres.name
-FROM tv_shows
+SELECT tv_genres.name
+FROM tv_genres
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 JOIN genres ON tv_show_genres.genre_id = genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
