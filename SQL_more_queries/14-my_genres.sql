efc5a32a35d31c07cd8ca0fde4bd270ce23b4292
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-15 21:46:34 -0500

    tv genre and tv sows fixed

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- import from tv shows, list all dexter shows
 SELECT tv_genres.name
-FROM tv_genres
+FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON tv_show_genres.genre_id = genres.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
