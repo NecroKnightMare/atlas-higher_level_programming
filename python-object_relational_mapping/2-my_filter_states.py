230c09d911b22dcf2a1dd860efbae08b88f23a33
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-16 04:08:42 +0000

    erased like

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ import sys
 def display_states(username, password, database, state_name):
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
-    parameters = "SELECT * FROM states WHERE name = LIKE '{}' ORDER BY id ASC".format(state_name)
+    parameters = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(parameters)
     states = cursor.fetchall()
     for state in states:
