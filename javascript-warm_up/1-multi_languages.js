f405a44b28d0fc6e899a95700acb3e9b9a388107
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-15 23:33:46 +0000

    used let to assign string and used console.log to print

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// using let instead of const
+let first = 'C is fun';
+let second = 'Python is cool';
+let third = 'JavaScript is amazing';
+console.log(first);
+console.log(second);
+console.log(third);
