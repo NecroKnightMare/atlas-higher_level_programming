76f0bb347c8ab4e26c4501863289787e514efd5c
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-15 23:27:20 +0000

    javascript uses single quotes, added comments for self

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing";
+// first file in javascript, print string
+// with constant and console.log()
+// javascript uses single quotes and semicolons
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
