0bc64497af8d9399afb4fb0c7fe552f50f8f3b67
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:56:58 +0000

    created is const for string

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -6,7 +6,8 @@ const process = require('process');
 if (process.argv.length > 4) {
     const arg1 = process.argv[2];
     const arg2 = process.argv[3];
-    console.log('${arg1} is ${arg2}')
+    const arg3 = "is"
+    console.log(arg1, arg3, arg2)
 }
 else if (process.argv[1]) {
     // console.log('Argument found'); don't need this for the checker
