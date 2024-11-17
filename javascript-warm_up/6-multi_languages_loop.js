dcae83ff6d9a22215de30878edb52fde4d5a18a2
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 19:21:36 +0000

    prints array of strings in order with while loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints array of strings in order with while loop
+const process = require('process');
+const arg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+while (arg) {
+  console.log(arg[0], arg[1], arg[2])
+} 
