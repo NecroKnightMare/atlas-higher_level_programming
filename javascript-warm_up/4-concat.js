580bd3ad9925af3a5860f67acfd6a6ab0a5bbf1e
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:58:09 +0000

    seeing if its in the wrong block scope

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -11,7 +11,10 @@ if (process.argv.length > 4) {
 }
 else if (process.argv[1]) {
     // console.log('Argument found'); don't need this for the checker
-    console.log(process.argv[2]);
+    const arg1 = process.argv[2];
+    const arg2 = process.argv[3];
+    const arg3 = "is"
+    console.log(arg1, arg3, arg2)
 }
 else {
     console.log('No argument');
