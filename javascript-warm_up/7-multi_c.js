76c10943b667808c093cc7b0f18566d9c3f6d94a
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 21:33:22 +0000

    confused on the wording, thought if else wasnt allowed, added else

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,11 +3,10 @@
 const process = require('process');
 const arg = process.argv[2];
 const stringToInt = parseInt(arg);
-while (!isNaN(stringToInt)) { 
+if (isNaN(stringToInt)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let x = 0; x < stringToInt; x++) {
     console.log('C is fun');
 }
-}
-if (isNaN(stringToInt)) {
-  console.log('Missing number of occurrences');
 }
