e5c867ac7f08b82cc6fa15932901e3776d08db01
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:09:57 +0000

    commented line that prints the argument

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const process = require('process');
 if (process.argv.length > 2) {
     console.log('Argument found');
-    console.log(process.argv);
+    // console.log(process.argv); didn't need to print nut this is how
 }
 else {
     console.log('No argument');
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const process = require('process');
+if (process.argv.length == 1) {
+    console.log('Argument found');
+    console.log(process.argv);
+}
+else {
+    console.log('No argument');
+}
