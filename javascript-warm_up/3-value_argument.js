695e5c7130da364561b71e91a6c41460df199f63
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:18:05 +0000

    prints first arg

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-
+// same as file 2 but prints first argument
 const process = require('process');
 if (process.argv.length == 1) {
     console.log('Argument found');
     console.log(process.argv);
+}
+else if (process.argv.length > 2) {
+    console.log('Arguments found');
+    // console.log(process.argv); didn't need to print but this is how
 }
 else {
     console.log('No argument');
