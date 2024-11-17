cebd9653ad20c2c45e3ba9ed5f6283c6d2e0ec44
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:10:40 +0000

    syntax errors

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 // passing process as value to print if else conditions
 const process = require('process');
 if (process.argv.length > 2) {
-    console.log('Argument found');
-    // console.log(process.argv); didn't need to print nut this is how
+    console.log('Arguments found');
+    // console.log(process.argv); didn't need to print but this is how
 }
 else {
     console.log('No argument');
