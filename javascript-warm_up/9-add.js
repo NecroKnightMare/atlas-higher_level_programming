cb359c7d5c4b130c23b917bded2afe665209eda9
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 22:19:02 +0000

    added isNaN method and parse through for integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 // print sum of a + b
+const process = require('process');
 function add(a, b) {
-    console.log( a + b );
+    return a + b;
+}
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+if (isNaN(arg1) || isNaN(arg2)) {
+    console.log('NaN');
+} else {
+    console.log(add);
 }
