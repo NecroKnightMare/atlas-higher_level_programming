78b34c25d88524db56fff457edcfb5b005ea594c
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 22:28:07 +0000

    fixed indents

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,12 +2,12 @@
 // print sum of a + b
 const process = require('process');
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 if (isNaN(arg1) || isNaN(arg2)) {
-    console.log('NaN');
+  console.log('NaN');
 } else {
-    console.log(arg1 + arg2);
+  console.log(arg1 + arg2);
 }
