ef82c35e4fc5781c042c6df3aa69adb6fbb1c6f2
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 21:28:01 +0000

    added while loop

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,8 +3,10 @@
 const process = require('process');
 const arg = process.argv[2];
 const stringToInt = parseInt(arg);
- for (let x = 0; x < stringToInt; x++) {
-  console.log(stringToInt);
+while (!isNaN(stringToInt)) { 
+  for (let x = 0; x < stringToInt; x++) {
+    console.log('C is fun');
+}
 }
 if (isNaN(stringToInt)) {
   console.log('Missing number of occurrences');
