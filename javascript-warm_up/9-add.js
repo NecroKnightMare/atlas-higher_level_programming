3c5dcd58b41e35aec913f3d21360556dbb39bece
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 22:29:10 +0000

    removed function, not used

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // print sum of a + b
 const process = require('process');
-function add(a, b) {
-  return a + b;
-}
+
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 if (isNaN(arg1) || isNaN(arg2)) {
