231d1c98d630f156a7e2508b7f9f057c825c5d48
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 01:03:31 +0000

    adding Bool

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // passing argv as value to print if else conditions
-let args = process.argv
-if args:
+let arg = process.argv
+
+if arg === True:
     console.log('Argument found')
 else:
     console.log('No argument')
