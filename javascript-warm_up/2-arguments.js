692c5c3652a6dda2afeda5e2005780b1f9da1edc
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:13:28 +0000

    added else if didnt see that I had to do if for one

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 // passing process as value to print if else conditions
 const process = require('process');
-if (process.argv.length > 2) {
+if (process.argv.length == 1) {
+    console.log('Argument found');
+}
+else if (process.argv.length > 2) {
     console.log('Arguments found');
     // console.log(process.argv); didn't need to print but this is how
 }
