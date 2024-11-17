41447e11812fec0894d2ee0c1f8b8e01327f7974
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 21:46:27 +0000

    added rows and col

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 // 
+const { resolveMx } = require('dns');
 const process = require('process');
 const arg = process.argv[2];
 const size = parseInt(arg);
 if (isNaN(size)) {
     console.log('Missing number of occurrences');
   } else {
-    for (let x = 0; x < size; x++) {
-      console.log('x');
+    for (let row = 0; row < size; row++) {
+      console.log('x', row);
+      for (let col = 0; col < size; col++) {
+        console.log('x', col);
     }
   }
+}
