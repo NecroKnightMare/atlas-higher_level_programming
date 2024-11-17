6b84a2a10cf906d65bbd42fe9c09ff79719ea8d3
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 22:25:28 +0000

    indents

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,12 +4,12 @@ const process = require('process');
 const arg = process.argv[2];
 const size = parseInt(arg);
 if (isNaN(size)) {
-    console.log('Missing size');
-  } else {
-    for (let row = 0; row < size; row++) {
-      let line = '';
-      for (let col = 0; col < size; col++) {
-        line += 'X';
+  console.log('Missing size');
+} else {
+  for (let row = 0; row < size; row++) {
+    let line = '';
+    for (let col = 0; col < size; col++) {
+      line += 'X';
     }
     console.log(line);
   }
