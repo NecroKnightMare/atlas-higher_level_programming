a1717d5e97d2a3eb46a8fb81e329f750d1e69dfc
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 03:02:32 +0000

    semistandard validation errors; 2 indents instead of 4 { on seperate line forgot ; and '' singlequotes only

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,20 +2,23 @@
 // print two args passed using "is"
 
 const process = require('process');
-
-if (process.argv == 2) {
-    const arg1 = process.argv[2];
-    const arg2 = process.argv[3];
-    const arg3 = "is"
-    console.log(arg1, arg3, arg2)
+// indents are 2 spaces not 4 in javascript
+if (process.argv === 2) 
+{
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+  const arg3 = 'is';
+  console.log(arg1, arg3, arg2);
 }
-else if (process.argv[1]) {
-    // console.log('Argument found'); don't need this for the checker
-    const arg1 = process.argv[2];
-    const arg2 = process.argv[3];
-    const arg3 = "is"
-    console.log(arg1, arg3, arg2)
+else if (process.argv[1]) 
+{
+  // console.log('Argument found'); don't need this for the checker
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+  const arg3 = 'is';
+  console.log(arg1, arg3, arg2);
 }
-else {
-    console.log('No argument');
+else 
+{
+  console.log('No argument');
 }
