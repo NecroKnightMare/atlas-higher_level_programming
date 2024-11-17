c90ca88f8bbcfe39e7a7523d73bf982891823243
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 23:45:55 +0000

    added file 1 into 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// add constructor for heigght and width
+class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  
+  module.exports = Rectangle;
