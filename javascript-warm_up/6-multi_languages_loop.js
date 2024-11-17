9754e9ba7df8b2fa92eb5256c5c95dbf566e4238
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 19:27:29 +0000

    new line at end added, whitetrail rem, and process const removed

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // prints array of strings in order with while loop
 // will increment to avoid infinite loop
-const process = require('process');
 const arg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let index = 0;
 while (index < arg.length) {
   console.log(arg[index]);
   index++;
-} 
+}
