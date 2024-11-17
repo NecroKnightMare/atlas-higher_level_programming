f3c3936a5df676c5a803ebf7961dbe204a5bc852
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 00:53:45 +0000

    using arguments as value to see if args found, using console.log

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-if
+// passing argv as value to print if else conditions
+let args = process.argv
+if args:
+    console.log('Argument found')
+else:
+    console.log('No argument')
