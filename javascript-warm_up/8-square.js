ee0d5288a14cedea33847ee6cd6bfe8b708a5dc8
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 22:22:30 +0000

    wrong error message

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const process = require('process');
 const arg = process.argv[2];
 const size = parseInt(arg);
 if (isNaN(size)) {
-    console.log('Missing number of occurrences');
+    console.log('Missing size');
   } else {
     for (let row = 0; row < size; row++) {
       let line = '';
