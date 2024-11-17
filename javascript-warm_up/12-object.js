e4a64ca633be0f5525a5d0cc0267552bd48fe8dc
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 23:11:58 +0000

    update given script to change value from 12 to 89

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// update given script to change value from 12 to 89
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// my line of code
+myObject.value = 89;
+console.log(myObject);
