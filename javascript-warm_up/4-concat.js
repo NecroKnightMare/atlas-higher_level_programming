322fc21ca8cb22e2d8dc566b87a2d07b6a1c3d65
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 03:04:15 +0000

    nevermind- {} need to be on opening line and last line of block scope

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,22 +3,16 @@
 
 const process = require('process');
 // indents are 2 spaces not 4 in javascript
-if (process.argv === 2) 
-{
+if (process.argv === 2){
   const arg1 = process.argv[2];
   const arg2 = process.argv[3];
   const arg3 = 'is';
-  console.log(arg1, arg3, arg2);
-}
-else if (process.argv[1]) 
-{
+  console.log(arg1, arg3, arg2);}
+else if (process.argv[1]) {
   // console.log('Argument found'); don't need this for the checker
   const arg1 = process.argv[2];
   const arg2 = process.argv[3];
   const arg3 = 'is';
-  console.log(arg1, arg3, arg2);
-}
-else 
-{
-  console.log('No argument');
-}
+  console.log(arg1, arg3, arg2);}
+else {
+  console.log('No argument');}
