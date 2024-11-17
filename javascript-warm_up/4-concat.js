6a999683e8022361f32d31395a1194c1ffc8e28d
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-17 02:53:04 +0000

    wrong operator

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 
 const process = require('process');
 
-if (process.argv.length < 4) {
+if (process.argv.length > 4) {
     const arg1 = process.argv[2];
     const arg2 = process.argv[3];
     console.log('${arg1} is ${arg2}')
