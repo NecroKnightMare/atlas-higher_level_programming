216693219e97e78d093db9ed55a2bc8ea0196635
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 04:03:10 +0000

    ; added

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // square class that inherits from Rectangle
-const Rectangle = require('./4-rectangle')
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
