58cba4bf0bd6503516632ccea9abefa878d1010f
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 03:43:44 +0000

    fixed indents

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -2,29 +2,29 @@
 // add instance to rotate w and h
 // add instance to multipy w and h by 2
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-
-    rotate () {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-
-    double () {
-      this.width *= 2;
-      this.height *= 2;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
 module.exports = Rectangle;
   
