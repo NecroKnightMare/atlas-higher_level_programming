2cef32fb3a9d9e26787e296bfb624b968836b54c
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 23:58:54 +0000

    semistandard fixed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 // passing process as value to print if else conditions
 const process = require('process');
 if (process.argv.length == 3) {
-    console.log('Argument found');
-}
-else if (process.argv.length > 3) {
-    console.log('Arguments found');
-    // console.log(process.argv); didn't need to print but this is how
-}
-else {
-    console.log('No argument');
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+  // console.log(process.argv); didn't need to print but this is how
+} else {
+  console.log('No argument');
 }
