5b9cba315246928c17e06c84afcf801b4137e8d5
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 01:43:19 +0000

    added if statement

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,10 @@
 // add constructor for heigght and width
 class Rectangle {
     constructor (w, h) {
-      this.width = w;
-      this.height = h;
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
     }
   }
-
-  
+}
   module.exports = Rectangle;
