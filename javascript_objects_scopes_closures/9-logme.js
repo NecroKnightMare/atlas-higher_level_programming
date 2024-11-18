d16f333fd7769b8f02288965ca43e45b9f2b42a7
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 23:28:13 +0000

    condition tha increments

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-exports.logMe = function (item);
-<number arguments already printed>: <current argument value></current>
+// increment throigh arguments printed
+// and new argument value
+let count = 0;
+exports.logMe = function (item) {
+    console.log(`${count}: ${item}`);
+    count++;
+}
