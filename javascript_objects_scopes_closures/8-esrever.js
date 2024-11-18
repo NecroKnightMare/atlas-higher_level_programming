4d1b4b7e3200b93baeaf62e6338eaaeffd34ec1e
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 23:19:19 +0000

    returns list in reverse added permissions

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-exports.esrever = function (list);
+// returns list in reverse
+exports.esrever = function (list) {
+  const rev_list = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    rev_list.push(list[i]);
+  }
+  return rev_list;
+};
