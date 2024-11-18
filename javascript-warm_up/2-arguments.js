55b21aa3e5c845aa8f40ea4db399f88f84ce8740
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 23:57:44 +0000

    changed length of arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // passing process as value to print if else conditions
 const process = require('process');
-if (process.argv.length == 1) {
+if (process.argv.length == 3) {
     console.log('Argument found');
 }
-else if (process.argv.length > 2) {
+else if (process.argv.length > 3) {
     console.log('Arguments found');
     // console.log(process.argv); didn't need to print but this is how
 }
