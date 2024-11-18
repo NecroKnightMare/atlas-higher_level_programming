0c5fed5bad3c6a2f8677bc61cc4b56b60d05df01
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-18 03:42:12 +0000

    add instance to rotate w and h add instance to multipy w and h by 2

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// add instance to print X
+// add instance to rotate w and h
+// add instance to multipy w and h by 2
 class Rectangle {
     constructor (w, h) {
       if (w > 0 && h > 0) {
@@ -7,20 +8,23 @@ class Rectangle {
         this.height = h;
       }
     }
-  
+
     print () {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
       }
     }
-    
-    rotate () {
 
+    rotate () {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
     }
 
     double () {
-        
+      this.width *= 2;
+      this.height *= 2;
     }
   }
-  module.exports = Rectangle;
+module.exports = Rectangle;
   
