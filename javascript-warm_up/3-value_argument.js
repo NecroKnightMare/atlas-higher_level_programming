c6b7f9871f7c8a68dbfc5075d716430683dbe91f
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-19 00:01:23 +0000

    had wrong index, 1 is path, 2=firstarg, fixed semistandard

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 //  prints first argument and doesn't use length
 const process = require('process');
-if (process.argv[1]) {
-    // console.log('Argument found'); don't need this for the checker
-    console.log(process.argv[2]);
-}
-else if (process.argv[2]) {
-    console.log('Arguments found');
-    // console.log(process.argv); didn't need to print but this is how
-}
-else {
-    console.log('No argument');
-
+if (process.argv[2]) {
+  // console.log('Argument found'); don't need this for the checker
+  console.log(process.argv[2]);
+} else if (process.argv[2]) {
+  console.log('Arguments found');
+  // console.log(process.argv); didn't need to print but this is how
+} else {
+  console.log('No argument');
 }
