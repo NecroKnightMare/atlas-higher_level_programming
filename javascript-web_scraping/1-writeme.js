95a95468998c19cc1b46f829f03d756f3a7182e4
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-21 23:18:03 +0000

    Write a script that writes a string to the file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Write a script that writes a string to the file
+const fs = require('fs');
+// The first argument is the file path
+const filePath = process.argv[2];
+// string
+const phrase = 'Python is cool';
+// The content of the file must be read in utf-8
+fs.writeFile(filePath, 'utf-8', phrase, (err, data) => {
+// If an error occurred during the reading, print the error object
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
