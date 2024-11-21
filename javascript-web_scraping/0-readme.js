625544455eecbc63927dcddf0010fa721f20c236
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-21 23:02:25 +0000

    added process for argv, deleted err message

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,11 +2,12 @@
 // Write a script that reads and prints the content of a file.
 const fs = require('fs');
 // The first argument is the file path
+const filePath = process.argv[2];
 // The content of the file must be read in utf-8
-fs.readFile('0-readme.js', 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
 // If an error occurred during the reading, print the error object
   if (err) {
-  console.error('doesntexist', err);
+  console.error(err);
     return;
   }
   console.log(data);
