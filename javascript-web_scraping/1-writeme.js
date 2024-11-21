1fdbf12bd146c62bac814614729bd1888ecde2da
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-21 23:22:49 +0000

    got rid of data

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -6,11 +6,10 @@ const filePath = process.argv[2];
 // string
 const phrase = process.argv[3];
 // The content of the file must be read in utf-8
-fs.writeFile(filePath, 'utf-8', phrase,  (err, data) => {
+fs.writeFile(filePath, 'utf-8', phrase,  (err) => {
 // If an error occurred during the reading, print the error object
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
 });
