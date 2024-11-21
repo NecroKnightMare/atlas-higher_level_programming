5ad4e0aa4aae53297a539a34fbc325a25217da46
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-21 22:49:32 +0000

    created file to read and print contents of the file, readme filled with learning objectives and requirements

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+const fs = require('fs');
+// The first argument is the file path
+// The content of the file must be read in utf-8
+fs.readFile('0-readme.js', 'utf-8', (err, data) => {
+// If an error occurred during the reading, print the error object
+  if (err) {
+  console.error('doesntexist', err);
+    return;
+  }
+  console.log(data);
+});
