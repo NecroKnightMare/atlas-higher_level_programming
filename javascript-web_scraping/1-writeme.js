e5442cd6ff8f6687b85e694427f39bcc0d889b90
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-21 23:23:12 +0000

    erased whietrail

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const filePath = process.argv[2];
 // string
 const phrase = process.argv[3];
 // The content of the file must be read in utf-8
-fs.writeFile(filePath, 'utf-8', phrase,  (err) => {
+fs.writeFile(filePath, 'utf-8', phrase, (err) => {
 // If an error occurred during the reading, print the error object
   if (err) {
     console.error(err);
