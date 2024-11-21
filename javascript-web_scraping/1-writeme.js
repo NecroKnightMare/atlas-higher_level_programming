ef19a8a2f305f9b2f007234aeb69c120c578a9b5
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-21 23:37:27 +0000

    added encoding to clarify to checker the string is not the encoding arg

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,10 +3,11 @@
 const fs = require('fs');
 // The first argument is the file path
 const filePath = process.argv[2];
-// string
-const phrase = process.argv.slice(3).join(' ');
+// string argument
+const phrase = process.argv[3];
 // The content of the file must be read in utf-8
-fs.writeFile(filePath, 'utf-8', phrase, (err) => {
+// added encoding, thinks string is being interpreted as the encoder
+fs.writeFile(filePath, phrase, { encoding: 'utf-8' }, (err) => {
 // If an error occurred during the reading, print the error object
   if (err) {
     console.error(err);
