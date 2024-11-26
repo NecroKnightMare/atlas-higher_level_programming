087e9687dc66c8f448df138c113e4138102dbb28
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-26 03:34:54 +0000

    same as file 0 but cant use query selector, used core functions instead

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// same as o but You can’t use document.querySelector to select the HTML tag
+$(document).ready(function() {
+    // use core function $
+    $('header').css('color', '#FF0000');
+});
