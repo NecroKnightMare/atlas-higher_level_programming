198ad453d71d7ef2e471e07c13a5baaad8dea027
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-26 03:29:17 +0000

    new directory; first file to change color of the header to red; and added test file

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a JavaScript script that updates 
+// the text color of the <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', (event) => {
+  // select the HTML tag with document.querySelector
+  document.querySelector('header').style;event.color = '#FF0000';
+});
