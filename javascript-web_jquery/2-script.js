bf72279eb966071ecf0f81a67a5cd16d6afca1ec
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-27 00:17:45 +0000

    file 1 changes text color; file 2 changes element color

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -2,7 +2,8 @@
 $(document).ready(function() {
   // use core from file 1
   // use click and specify red_header div
-  $('red_header').click(function() {
+  $('#red_header').click(function() {
+    // change text color
     $('header').css('color', '#FF0000');
     });
 });
