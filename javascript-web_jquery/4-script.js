34bc380858bf82601b6db971432791202aa2b9df
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-27 00:27:18 +0000

    permissions for 3; toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+$(document).ready(function() {
+    // use click to activate event to toggle
+    $('#toggle_header').click(function() {
+      // if header is red or green do the following
+      if ($('header').hasClass('red')) {
+        // if red remove and add green
+        $('header').removeClass('red').addClass('green');
+      } else {
+        $('header').removeClass('green').addClass('red');
+      }  
+    });
+  });
