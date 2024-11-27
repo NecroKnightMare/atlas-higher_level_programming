f432eed68a15bd9f7e560689c4328005fe230873
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-27 00:29:17 +0000

    semistandard fixes

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 $(document).ready(function() {
-    // use core from file 1
-    // use click and specify red_header div
-    $('#red_header').click(function() {
-      // change header element
-      $('header').css('background-color', '#FF0000');
-      });
+  // use core from file 1
+  // use click and specify red_header div
+  $('#red_header').click(function() {
+    // change header element
+    $('header').css('background-color', '#FF0000');
   });
+});
