a9271ba96e13cd8f3eb3ed1ae1ad37cee934c459
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-27 03:14:38 +0000

    simplified if statement and used some function to specify

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,24 +6,20 @@ const movie = process.argv[2];
 // Wedge Antilles character
 const character = '18';
 // API URL
-const url = `https://swapi-api.hbtn.io/api/people/18`;
+// const url = `https://swapi-api.hbtn.io/api/people/18`;
 request(movie, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
   const films = JSON.parse(body).results;
-  console.log(films.length);
-});
-//   let count = 0;
+  let count = 0;
 
-//   films.forEach((film) => {
-//     film.characters.forEach((characterUrl) => {
-//       if (characterUrl.includes(url)) {
-//     count++;
-//       }
-//     });
-//   });
-//     // grab count of wedge Antilles movies that are present
-//   console.log(count);
-// });
+  films.forEach((film) => {
+    if (film.characters.some(characterUrl => characterUrl.includes(character))) {
+      count++;
+    }
+  });
+  // grab count of wedge Antilles movies that are present
+  console.log(count);
+});
