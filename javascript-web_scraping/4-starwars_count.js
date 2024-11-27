23072d126547268f6b6ef8817e3b58373e9ed559
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-27 03:07:30 +0000

    trying a simplified, grabbing length and grabbing char id 18

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,22 +6,24 @@ const movie = process.argv[2];
 // Wedge Antilles character
 const character = '18';
 // API URL
-const url = `https://swapi-api.hbtn.io/api/people/${character}`;
+const url = `https://swapi-api.hbtn.io/api/people/18`;
 request(movie, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
   const films = JSON.parse(body).results;
-  let count = 0;
-
-  films.forEach((film) => {
-    film.characters.forEach((characterUrl) => {
-      if (characterUrl.includes(url)) {
-    count++;
-      }
-    });
-  });
-    // grab count of wedge Antilles movies that are present
-  console.log(count);
+  console.log(films.length);
 });
+//   let count = 0;
+
+//   films.forEach((film) => {
+//     film.characters.forEach((characterUrl) => {
+//       if (characterUrl.includes(url)) {
+//     count++;
+//       }
+//     });
+//   });
+//     // grab count of wedge Antilles movies that are present
+//   console.log(count);
+// });
